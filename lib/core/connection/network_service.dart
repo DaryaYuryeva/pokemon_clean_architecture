ba7b988f6fefc142a54d4926259b0b875f43893c
Darya Yuryeva <79998801+DaryaYuryeva@users.noreply.github.com>
2024-10-09 17:51:49 +0300

    gradle upd

--- lib/core/connection/network_service.dart
+++ lib/core/connection/network_service.dart
@@ -3,14 +3,14 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 class NetworkService {
   final Connectivity _connectivity = Connectivity();
 
-  Stream<ConnectivityResult> get connectivityStream =>
+  Stream<List<ConnectivityResult>> get connectivityStream =>
       _connectivity.onConnectivityChanged;
 
   Future<bool> get isConnected => _checkIsConnected();
 
   Future<bool> _checkIsConnected() async {
     final result = await _connectivity.checkConnectivity();
-    return result == ConnectivityResult.mobile ||
-        result == ConnectivityResult.wifi;
+    return result.last == ConnectivityResult.mobile ||
+        result.last == ConnectivityResult.wifi;
   }
 }
